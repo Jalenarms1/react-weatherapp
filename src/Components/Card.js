3d8d6e80e6d92f4ccfbad06d37ad7509a98a2734
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-13 23:17:55 -0700

    saving progress

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,17 +7,25 @@ const Card = () => {
 
     const [city, setCity] = useState("");
     const [showCity, setShowCity] = useState(false)
-    const [weatherStats, setWeatherStats] = useState({})
+    const [weatherStats, setWeatherStats] = useState(false)
+    const [showError, setShowError] = useState(false)
 
     function handleChange (e) {
         setCity(e.target.value)
     }
 
     async function handleSubmit(e){
+        setShowError(false)
         e.preventDefault();
         let resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ec5c7380ad1c206f8903e05d34e299ad&units=imperial`,
         {method: 'GET'})
+        if(resp.status === 404){
+            setShowError(true)
+            return 
+        }
+        
         let data = await resp.json();
+        
 
         setWeatherStats(data)
         setShowCity(true)
@@ -29,11 +37,11 @@ const Card = () => {
 
     return (
         <>
-            <div className="bg-primary cst-card m-auto" style={{width: '60%', height: '25rem'}}>
-                <div className="card-heading card-heading-bg">
-                    <Form onChange={handleChange} handleSubmit={handleSubmit} name={weatherStats.name} />
+            <div className="bg-primary cst-card m-auto" style={{width: '60%', height: '25rem', borderRadius: '5px'}}>
+                <div className="card-heading card-heading-bg" style={{borderRadius: '5px'}}>
+                    <Form showError={showError} onChange={handleChange} handleSubmit={handleSubmit} name={weatherStats.name} />
                 </div>
-                <WeatherDetails stats={weatherStats} show={showCity} />
+                {weatherStats ? <WeatherDetails stats={weatherStats} show={showCity}/> : <h1 className="w-100 m-auto">Search for a city</h1>}
                     
             </div>
         </>
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -7,13 +7,16 @@ const Form = (props) => {
             <form onSubmit={props.handleSubmit}>
                 <div className="wrap-input d-flex justify-content-between w-100 p-4">
                     <div className="wrap-input d-flex">
-                        <input type='text' className="p-1" placeholder="Enter a city name..." onChange={props.onChange} name="city" />
+                        <input type='text' className="p-1" placeholder="Enter a city name..." onChange={props.onChange} name="city" style={{border: props.showError ? '1px solid red' : 'none'}} required />
                         <button className="btn btn-outline-warning mx-2">Submit</button>
                     </div>
                     <h4 className="text-warning">Current city: {props.name}</h4>
                 </div>
-            </form>
+                <div className="w-50 d-flex align-items-center">
+                    {props.showError && <small className="px-4 mb-2" style={{color: 'red', fontSize: '.75em'}}>Please enter a valid city name</small>}
 
+                </div>
+            </form>
 
         </>
     )
